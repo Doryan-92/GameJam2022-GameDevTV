5484805c6ce03fd169421e456e1f0a081348f483
Doryan-92 <105790343+Doryan-92@users.noreply.github.com>
2022-05-22 00:06:41 +0200

    PlayerControl Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,13 +11,14 @@ public class PlayerController : MonoBehaviour
     bool isOnGround = true;
     public bool gameOver = false;
     float gravityModifier;
+    float fallTime;
 
 
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GameObject.Find("Player").GetComponent<Rigidbody2D>();
-
+        fallTime = 0;
         Physics.gravity *= gravityModifier;
         }
 
@@ -29,13 +30,32 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector2.right * Time.deltaTime * speed * horizontalInput);
 
 
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        if (Input.GetKeyDown(KeyCode.Space) && !Input.GetKey(KeyCode.DownArrow) && isOnGround && !gameOver)
             {
             isOnGround = false;
             playerRB.AddForce(Vector3.up * jumpingForce, ForceMode2D.Impulse);
             }
 
-    }
+        if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.Space) && isOnGround)
+            {
+       
+            gameObject.GetComponent<Collider2D>().enabled = false;
+            if(fallTime == 0)
+                {
+                fallTime += Time.deltaTime;
+
+                }
+            else if(fallTime >= 1) //non funziona
+                {
+                Debug.Log("collider enabled!");
+                gameObject.GetComponent<Collider2D>().enabled = true;
+                fallTime = 0;
+                }
+
+
+            }
+
+        }
     private void OnCollisionEnter2D(Collision2D collision)
         {
         if (collision.gameObject.CompareTag("Ground"))
